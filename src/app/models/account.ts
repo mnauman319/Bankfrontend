a7a61427b612aae2cec0a77562b814a9e04c4b46
mnauman319 <mnauman319@gmail.com>
2020-10-05 16:02:05 -0500

    Added all the controller calls to the AccountService. Started to implement them in the home component

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,9 +1,9 @@
 export class Account{
 
-    private aId:number;
-    private cId:number;
-    private name:string;
-    private balance:number;
+    aId:number;
+    cId:number;
+    name:string;
+    balance:number;
 
     constructor(aId:number,cId:number,name:string,balance:number){
         this.aId = aId;
@@ -11,37 +11,4 @@ export class Account{
         this.name = name;
         this.balance = balance;
     }
-
-    public getAId(): number {
-        return this.aId;
-    }
-
-    public setAId(aId: number): void {
-        this.aId = aId;
-    }
-
-    public getCId(): number {
-        return this.cId;
-    }
-
-    public setCId(cId: number): void {
-        this.cId = cId;
-    }
-
-    public getName(): string {
-        return this.name;
-    }
-
-    public setName(name: string): void {
-        this.name = name;
-    }
-
-    public getBalance(): number {
-        return this.balance;
-    }
-
-    public setBalance(balance: number): void {
-        this.balance = balance;
-    }
-
 }
--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,9 +1,42 @@
+import { Account } from '../models/account';
+import { CustomerService } from './customer.service';
+import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  
+  
+  constructor(private httpClient:HttpClient, private cserv:CustomerService) { }
+  cId:number = this.cserv.loggedInCustomer.getCId();
+  baseUrl:string = environment.baseUrl + `/customers/${this.cId}/accounts`;
+  async getAllAccounts():Promise<Account[]>{
+    return await this.httpClient.get<Account[]>(`${this.baseUrl}`).toPromise();
+  }
+  async getAllAccountsLessThan(value:number):Promise<Account[]>{
+    return await this.httpClient.get<Account[]>(`${this.baseUrl}?balancelessthan=${value}`).toPromise();
+  }
+  async getAllAccountsGreaterThan(value:number):Promise<Account[]>{
+    return await this.httpClient.get<Account[]>(`${this.baseUrl}?balancegreaterthan=${value}`).toPromise();
+  }
+  async getAllAccountsLessThanAndGreaterThan(value1:number,value2:number):Promise<Account[]>{
+    return await this.httpClient.get<Account[]>(`${this.baseUrl}?balancelessthan=${value1}&balancegreaterthan=${value2}`).toPromise();
+  }
+  async getAccountById(aId:number):Promise<Account>{
+    return await this.httpClient.get<Account>(`${this.baseUrl}/${aId}`).toPromise();
+  }
+  async createAccount(account:Account):Promise<Account>{
+    return await this.httpClient.post<Account>(`${this.baseUrl}`,account).toPromise();
+  }
+  async updateAccount(account:Account):Promise<Account>{
+    return await this.httpClient.put<Account>(`${this.baseUrl}`,account).toPromise();
+  }
+  async deleteAccount(aId:number):Promise<boolean>{
+    return await this.httpClient.delete<boolean>(`${this.baseUrl}/aId`).toPromise();
+  }
 
-  constructor() { }
+  
 }
